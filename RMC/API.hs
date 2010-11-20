6d3636573502ca186fa810b5a6701b1cbac6794d
fallen <fallen.s4e@gmail.com>
2010-11-20 23:34:04 +0600

    commit

--- RMC/API.hs
+++ RMC/API.hs
@@ -1,15 +1,25 @@
-module API where
+module RMC.API where
 
 import Data.Time
+import Control.Monad
 
 import RMC.Protobuf.RMC.RMC 	as RMC
 
--- fromGregorian :: Integer -> Int -> Int -> Day
-rmcToDay :: RMC -> Day
-rmcToDay = undefined
+rmcToDay :: RMC -> Maybe Day
+rmcToDay = liftM (f . fromIntegral) . date
+    where f :: Int -> Day
+          f num = fromGregorian (fromIntegral years) (fromIntegral months) (fromIntegral days)
+              where years  = num `div` (12*31)
+                    months = (num - years*12*31) `div` 31
+                    days   = (num - years*12*31) - months*31
 
--- secondsToDiffTime :: Integer -> DiffTime
--- picosecondsToDiffTime :: Integer -> DiffTime
-rmcToUTCTime :: RMC -> UTCTime
-rmcToUTCTime rmc = UTCTime (rmcToDay rmc) (secondsToDiffTime (undefined rmc))
+rmcToDiffTime :: RMC -> Maybe DiffTime
+rmcToDiffTime = liftM mkTime . time
+    where mkTime :: Double -> DiffTime
+          mkTime = picosecondsToDiffTime . truncate . (* 10^12)
 
+rmcToUTCTime :: RMC -> Maybe UTCTime
+rmcToUTCTime rmc = mkUTCTime (rmcToDay rmc) (rmcToDiffTime rmc)
+    where mkUTCTime day time = case (day, time) of
+                                 (Just d, Just t)  -> Just $ UTCTime d t
+                                 _		   -> Nothing
---
+++ Tracker/Tracker.hs
@@ -0,0 +1,8 @@
+module Tracker.Tracker where
+-- Tracker emulation
+
+import Test.QuickCheck
+import Data.List
+
+main :: IO ()
+main = undefined
