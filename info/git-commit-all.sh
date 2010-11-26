8e5c37937be09929a5ea1fa816fe19cea984383d
fallen <fallen.s4e@gmail.com>
2010-11-26 01:45:47 -0500

    commit

--- info/git-commit-all.sh
+++ info/git-commit-all.sh
@@ -1,6 +1,8 @@
 cd d:/projects/haskell/GPS-stream/
 find . -type f -name "*#" -exec rm -f {} \;
 find . -type f -name "*~" -exec rm -f {} \;
+find . -type f -name "*.o" -exec rm -f {} \;
+find . -type f -name "*.hi" -exec rm -f {} \;
 git add --all
 git commit -m 'commit'
 git push origin master
