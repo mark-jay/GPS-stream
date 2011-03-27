a193c9e940a612b0afa7a24798248530135b3d43
fallen <fallen@fallen-laptop.(none)>
2011-03-27 16:44:34 +0600

    commit

--- sandbox/2/Relations/Timable.hs
+++ sandbox/2/Relations/Timable.hs
@@ -1,5 +1,9 @@
 {-# LANGUAGE TemplateHaskell, FlexibleInstances #-}
-module Timable where
+module Timable( mkTimable
+              , getData
+              , TimableData
+              , Timable(..)
+              ) where
 
 import Data.Time.Clock
 import Data.Time
@@ -11,3 +15,15 @@ class Timable t where
     toTime :: t -> UTCTime
 
 $(timableInstances 10)
+
+-- TimableData
+newtype TimableData a = TimableData (UTCTime, a)
+
+mkTimable :: UTCTime -> a -> TimableData a
+mkTimable time a = TimableData (time, a)
+
+getData :: TimableData a -> a
+getData (TimableData (_, data')) = data'
+
+instance Timable (TimableData a) where
+    toTime (TimableData (time, _)) = time
